905ac7be216ef207ff3124117d30ed9795e58abb
cha-create <boingr66@gmail.com>
2024-12-27 23:48:46 -0600

    Fully working implementation of CGOL. Check readme. I get by with a little help from my friends.

---
+++ CGOL/README.md
@@ -0,0 +1,4 @@
+Conway's Game of Life.
+This is a simulation I wrote for CGOL implemented in C++. It's not perfect but it will allow you to make gliders and stuff, so good enough
+for me. You can use any tool to build it because it's just a standard C++ file. I used G++ from the GNU compiler and it works like a charm.
+Have fun!
--- CGOL/game.cpp
+++ CGOL/game.cpp
@@ -4,14 +4,19 @@
 #include <thread>
 using namespace std;
 int simCount = 0;
-int simLength = 15;
+int simLength = 0;
+int mainDelay = 0;
+int userOpt;
+
+//TODO: Add user-definable grid size
+
 int mainArr[8][8] = {
   {0, 0, 0, 0, 0, 0, 0, 0 },
   {0, 0, 0, 0, 0, 0, 0, 0 },
   {0, 0, 0, 0, 0, 0, 0, 0 },
-  {0, 0, 0, 0, 1, 1, 0, 1 },
-  {0, 0, 0, 0, 1, 0, 0, 0 },
-  {0, 0, 0, 0, 0, 1, 0, 0 },
+  {0, 0, 1, 0, 0, 0, 0, 0  },
+  {0, 0, 1, 1, 1, 0, 0, 0 },
+  {0, 0, 1, 0, 0, 0, 0, 0 },
   {0, 0, 0, 0, 0, 0, 0, 0 },
   {0, 0, 0, 0, 0, 0, 0, 0 }
 };
@@ -37,6 +42,16 @@ int offsetMatrix[8][2] = {
   {0, 1},
   {1, 1}
 };
+
+void promptUser() {
+  cout << "Welcome to Conway's Game of Life!" << endl;
+  cout << "How many simulation loops do you want?: ";
+  cin >> simLength;
+  cout << "How fast do you want the sim to run?(0-9; 9 is fastest): ";
+  cin >> userOpt;
+  mainDelay = (250 - (20 * userOpt));
+  return;
+}
 
 void draw() {
   for(int i = 0; i < 8; i++) {
@@ -67,21 +82,18 @@ void copyArray(int (&array1)[8][8], int (&array2)[8][8]) {
   }
 }
 
-int applyRulesToCell(int (&array)[8][8],  int x, int y, int numOn) {
-  int value = 0;
-  if(numOn < 2) {
-    value = 0;
-  }
-  else if(numOn > 3){
-    value = 0;
-  }
-  else if(array[y][x] == 0 && numOn > 1) {
-    value = 1;
-  }
-  array[y][x] = value;
-  return value;
+int applyRulesToCell(int currentState, int numOn) {
+    if (numOn < 2 || numOn > 3) {
+        return 0; // Cell dies or stays dead
+    }
+    if (currentState == 1 && (numOn == 2 || numOn == 3)) {
+        return 1; // Cell survives
+    }
+    if (currentState == 0 && numOn == 3) {
+        return 1; // Cell becomes alive
+    }
+    return 0; // Default case (cell stays dead)
 }
-
 void update() {
   //main game logic
   for(int x = 0; x < 8; x++) {
@@ -93,33 +105,32 @@ void update() {
             for(int i = 0; i < 8; i++) {
               int checkedX = (x + offsetMatrix[i][0]);
               int checkedY = (y + offsetMatrix[i][1]);
-              if(currentArr[checkedY][checkedX] == 1) {
+              if(mainArr[checkedY][checkedX] == 1) {
                 onCount += 1;
               }
               else {
                 offCount += 1;
               }
             }
-          applyRulesToCell(currentArr, x, y, onCount);
-        }
+            currentArr[y][x] = applyRulesToCell(mainArr[y][x], onCount);        }
       }
   }
 }
 }
 // Game logic runs as follows:
-// Clear currentArr
 // Draw current mainArr
 // Copy mainArr to currentArr
 // Apply logic to currentArr
 // Copy currentArr to mainArr
-int main() {
+int main() { 
+  promptUser();
   while(simCount <= simLength) {
     clearArray(currentArr);
     draw();
     copyArray(mainArr, currentArr);
     update();
     copyArray(currentArr, mainArr);
-    this_thread::sleep_until(chrono::system_clock::now() + chrono::milliseconds(250));
+    this_thread::sleep_until(chrono::system_clock::now() + chrono::milliseconds(mainDelay));
     cout << "Simulation Step: " << simCount << endl;
     simCount++;
     cout << endl;
